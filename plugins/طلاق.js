8d50f0d7c2322127da991783f553b84a471faa5f
ajamavdv <140681264+ajamavdv@users.noreply.github.com>
2024-04-28 03:28:03 +0100

    Update طلاق.js

--- plugins/طلاق.js
+++ plugins/طلاق.js
@@ -1,17 +1,17 @@
 let toM = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)},  وش رأيك في ثالثة😂*
-*${toM(b)},*لاتزعلي 🤭\n\*نزوجك مارك وش رأيك*`, null, {
-mentions: [a, b]
-})}
+  let ps = groupMetadata.participants.map(v => v.id)
+  let a = ps.getRandom()
+  let b
+  do b = ps.getRandom()
+  while (b === a)
+  m.reply(`*${toM(a)}, اعلن انفصالكم 😮‍💨*
+*${toM(b)},* \n\*~إذا كنت ترى أنهما غير ملائمين لبعضهما مثلي، فأخبرنا~🫣*`, null, {
+    mentions: [a, b]
+  })
+}
 handler.help = ['formarpareja']
 handler.tags = ['main', 'fun']
 handler.command = ['الطلاق','طلاق']
 handler.group = true
 export default handler
-
