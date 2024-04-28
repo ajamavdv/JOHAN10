0d0f10fdedc46546b2b64377cc9bd27987ac937f
ajamavdv <140681264+ajamavdv@users.noreply.github.com>
2024-04-28 03:37:11 +0100

    Update زواج.js

--- plugins/زواج.js
+++ plugins/زواج.js
@@ -1,16 +1,29 @@
-let toM = a => '@' + a.split('@')[0]
+function getRandomParticipant(participants) {
+  const randomIndex = Math.floor(Math.random() * participants.length);
+  return participants[randomIndex];
+}
+
 function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)}, الف مبروك للعريس🥳✨*
-*${toM(b)}, الف مبروك للعروسه🥳✨*\n\*ملاحظه ده منشن عشوائي للمرح فقط اذ لم يعجبك الأمر إذن ماتستخدمش الامر💗*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
-handler.tags = ['main', 'fun']
-handler.command = ['formarpareja','زواج']
-handler.group = true
-export default handler
+  const participants = groupMetadata.participants;
+  
+  if (participants.length < 2) {
+    m.reply('*عذرًا، يجب أن يكون هناك على الأقل شخصين في المجموعة لاختيار الزوج والزوجة.*');
+    return;
+  }
+  
+  const groom = getRandomParticipant(participants);
+  let bride;
+  
+  do {
+    bride = getRandomParticipant(participants);
+  } while (bride === groom);
+  
+  m.reply(`🎉 الف مبروك للعريس ${groom.id}! 🤵‍♂️\n🎉 الف مبروك للعروسة ${bride.id}! 👰‍♀️`);
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['formarpareja', 'زواج'];
+handler.group = true;
+
+export default handler;
