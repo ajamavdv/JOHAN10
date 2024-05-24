21935e7c6375aa072c43f4085a300fab65e2c5ad
ajamavdv <140681264+ajamavdv@users.noreply.github.com>
2024-05-24 02:18:55 +0100

    Update Attp.js

--- plugins/Attp.js
+++ plugins/Attp.js
@@ -1,32 +1,42 @@
-import fetch from 'node-fetch'
-import { sticker, addExif } from '../lib/sticker.js'
-import { Sticker } from 'wa-sticker-formatter'
-let handler = async(m, { conn, text, args, usedPrefix, command }) => {
-if (!text) throw `*⊱┇تحويل كلام الي ملصق┇⊰*\n\n*⟣「مثال」*\n*◉ ${usedPrefix + command} henry-Bot*`
-let teks = encodeURI(text)
+import fetch from 'node-fetch';
+import { createSticker } from '../lib/sticker.js';
+import { Sticker } from 'wa-sticker-formatter';
 
-if (command == 'ستك') {
-let a1 = await (await fetch(`https://api.erdwpe.com/api/maker/attp?text=${teks}`)).buffer()
-let a2 = await createSticker(a1, false, global.packname, global.author)
-conn.sendFile(m.chat, a2, 'sticker.webp', '', m, { asSticker: true })}
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!text) throw `*⊱┇تحويل كلام الى ملصق┇⊰*\n\n*⟣「مثال」*\n*◉ ${usedPrefix + command} henry-Bot*`;
+  let teks = encodeURI(text);
 
-if (command == 'ستك2') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ستك3') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+  if (command == 'ستك') {
+    let a1 = await (await fetch(`https://api.erdwpe.com/api/maker/attp?text=${teks}`)).buffer();
+    let a2 = await createSticker(a1, false, global.packname, global.author);
+    conn.sendFile(m.chat, a2, 'sticker.webp', '', m, { asSticker: true });
+  }
 
-if (command == 'ستك5') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ستكر4') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ستكر3') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ستكر2') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
-    
-if (command == 'ستكر') {
-conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true })}
+  if (command == 'ستتك2') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتك3') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/attp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتك5') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp6?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتكر4') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp5?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتكر3') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp3?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتكر2') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp2?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+
+  if (command == 'ستتكر') {
+    conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/ttp?apikey=${lolkeysapi}&text=${teks}`, 'sticker.webp', '', m, { asSticker: true });
+  }
+};
